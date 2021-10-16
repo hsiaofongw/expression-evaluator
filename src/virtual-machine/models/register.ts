bf60140506a6a3d315f1917a1c9eab327b1c4693
hsiaofongw <hsiaofong.w@gmail.com>
2021-10-17 01:39:49 +0800

    chore: 保存进度

--- src/virtual-machine/models/register.ts
+++ src/virtual-machine/models/register.ts
@@ -13,8 +13,27 @@ export type IRegisterBatchManufactureSpecification = {
 @Injectable()
 export class RegisterFactory {
   createMany(specs: IRegisterBatchManufactureSpecification): Register[] {
-    return [];
+    const registers: Register[] = [];
+    for (let i = 0; i < specs.registerCounts; i++) {
+      registers.push(new Register(specs.registerSizeByte));
+    }
+
+    return registers;
   }
 }
 
-export class Register {}
+export class Register {
+  _buffer!: Buffer;
+
+  constructor(private _size: number) {
+    this._buffer = Buffer.alloc(_size);
+  }
+
+  public read(): Buffer {
+    return this._buffer.slice(this._size);
+  }
+
+  public write(value: Buffer): void {
+    value.copy(this._buffer, 0, 0, this._size);
+  }
+}
