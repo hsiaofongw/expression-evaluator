cf39e7d6cdeca9d024bd6bd0ee0d1c7ffe004722
hsiaofongw <hsiaofong.w@gmail.com>
2022-03-04 19:42:35 +0800

    重构

--- src/lexer/config.ts
+++ src/lexer/config.ts
@@ -1,166 +1,138 @@
 import { TokenClass } from './interfaces';
 
-const leftParenthesis: TokenClass = {
-  name: 'leftParenthesis',
-  description: '左括号 (',
-};
+export const tokenClasses: Record<string, TokenClass> = {
+  leftParenthesis: {
+    name: 'leftParenthesis',
+    description: '左括号 (',
+  },
 
-const rightParenthesis: TokenClass = {
-  name: 'rightParenthesis',
-  description: '右括号 )',
-};
+  rightParenthesis: {
+    name: 'rightParenthesis',
+    description: '右括号 )',
+  },
 
-const plus: TokenClass = {
-  name: 'plus',
-  description: '加 +',
-};
+  plus: {
+    name: 'plus',
+    description: '加 +',
+  },
 
-const minus: TokenClass = {
-  name: 'minus',
-  description: '减 -',
-};
+  minus: {
+    name: 'minus',
+    description: '减 -',
+  },
 
-const times: TokenClass = {
-  name: 'times',
-  description: '乘 *',
-};
+  times: {
+    name: 'times',
+    description: '乘 *',
+  },
 
-const divideBy: TokenClass = {
-  name: 'divideBy',
-  description: '除 /',
-};
+  divideBy: {
+    name: 'divideBy',
+    description: '除 /',
+  },
 
-const dot: TokenClass = {
-  name: 'dot',
-  description: '点 .',
-};
+  dot: {
+    name: 'dot',
+    description: '点 .',
+  },
 
-const identifier: TokenClass = {
-  name: 'identifier',
-  description: '标识符',
-};
+  identifier: {
+    name: 'identifier',
+    description: '标识符',
+  },
 
-const number: TokenClass = {
-  name: 'digits',
-  description: '数字',
-};
+  number: {
+    name: 'digits',
+    description: '数字',
+  },
 
-const comma: TokenClass = {
-  name: 'comma',
-  description: '逗号 ,',
-};
+  comma: {
+    name: 'comma',
+    description: '逗号 ,',
+  },
 
-const leftSquareBracket: TokenClass = {
-  name: 'leftSquareBracket',
-  description: '左方括号 [',
-};
+  leftSquareBracket: {
+    name: 'leftSquareBracket',
+    description: '左方括号 [',
+  },
 
-const rightSquareBracket: TokenClass = {
-  name: 'rightSquareBracket',
-  description: '右方括号 ]',
-};
+  rightSquareBracket: {
+    name: 'rightSquareBracket',
+    description: '右方括号 ]',
+  },
 
-const leftBracket: TokenClass = {
-  name: 'leftBracket',
-  description: '左花括号 {',
-};
+  leftBracket: {
+    name: 'leftBracket',
+    description: '左花括号 {',
+  },
 
-const rightBracket: TokenClass = {
-  name: 'rightBracket',
-  description: '右花括号 }',
-};
+  rightBracket: {
+    name: 'rightBracket',
+    description: '右花括号 }',
+  },
 
-const doubleQuote: TokenClass = {
-  name: 'doubleQuote',
-  description: '双引号',
-};
+  doubleQuote: {
+    name: 'doubleQuote',
+    description: '双引号',
+  },
 
-const stringToken: TokenClass = {
-  name: 'string',
-  description: '字符串 Token',
-};
+  stringToken: {
+    name: 'string',
+    description: '字符串 Token',
+  },
 
-const assignToken: TokenClass = {
-  name: 'singleEqual',
-  description: '单等于号',
-};
+  assignToken: {
+    name: 'singleEqual',
+    description: '单等于号',
+  },
 
-const doubleEqualSign: TokenClass = {
-  name: 'doubleEqual',
-  description: '双等于号',
-};
+  doubleEqualSign: {
+    name: 'doubleEqual',
+    description: '双等于号',
+  },
 
-const tripleEqualSign: TokenClass = {
-  name: 'tripleEqualSign',
-  description: '仨等于号',
-};
+  tripleEqualSign: {
+    name: 'tripleEqualSign',
+    description: '仨等于号',
+  },
 
-const lessThanSign: TokenClass = {
-  name: 'lessThan',
-  description: '严格小于号',
-};
+  lessThanSign: {
+    name: 'lessThan',
+    description: '严格小于号',
+  },
 
-const lessThanOrEqualSign: TokenClass = {
-  name: 'lessThanOrEqualTo',
-  description: '严格不大于号',
-};
+  lessThanOrEqualSign: {
+    name: 'lessThanOrEqualTo',
+    description: '严格不大于号',
+  },
 
-const greaterThanSign: TokenClass = {
-  name: 'greaterThan',
-  description: '严格大于号',
-};
+  greaterThanSign: {
+    name: 'greaterThan',
+    description: '严格大于号',
+  },
 
-const greaterThanOrEqualTo: TokenClass = {
-  name: 'greaterThanOrEqualTo',
-  description: '严格不小于号',
-};
+  greaterThanOrEqualTo: {
+    name: 'greaterThanOrEqualTo',
+    description: '严格不小于号',
+  },
 
-const caretSign: TokenClass = {
-  name: 'caret',
-  description: '插入符',
-};
+  caretSign: {
+    name: 'caret',
+    description: '插入符',
+  },
 
-const percentSign: TokenClass = {
-  name: 'percent',
-  description: '百分号',
-};
+  percentSign: {
+    name: 'percent',
+    description: '百分号',
+  },
 
-const endOfLine: TokenClass = {
-  name: 'endOfLine',
-  description: '行结束符',
-};
+  endOfLine: {
+    name: 'endOfLine',
+    description: '行结束符',
+  },
 
-const endOfFile: TokenClass = {
-  name: 'endOfFile',
-  description: '文件结束符',
-};
-
-export const tokenClasses = {
-  plus,
-  minus,
-  times,
-  divideBy,
-  lessThanSign,
-  lessThanOrEqualSign,
-  greaterThanSign,
-  greaterThanOrEqualTo,
-  doubleEqualSign,
-  dot,
-  identifier,
-  number,
-  comma,
-  leftParenthesis,
-  rightParenthesis,
-  leftSquareBracket,
-  rightSquareBracket,
-  leftBracket,
-  rightBracket,
-  doubleQuote,
-  stringToken,
-  assignToken,
-  tripleEqualSign,
-  caretSign,
-  percentSign,
-  endOfLine,
-  endOfFile,
+  endOfFile: {
+    name: 'endOfFile',
+    description: '文件结束符',
+  },
 };
