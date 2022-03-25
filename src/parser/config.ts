321b89f893a068a576b4cb192ca29a5849ec5ae1
hsiaofongw <hsiaofong.w@gmail.com>
2022-03-26 00:29:21 +0800

    保存进度

--- src/parser/config.ts
+++ src/parser/config.ts
@@ -19,42 +19,41 @@ const Terminal: (name: TerminalSymbolType) => SyntaxSymbol = (name) => ({
 });
 
 export const sbl: Record<SymbolType, SyntaxSymbol> = {
-  start: NonTerminal('start'),
-  b1: NonTerminal('b1'),
-  b2: NonTerminal('b2'),
-  b3: NonTerminal('b3'),
-  b4: NonTerminal('b4'),
-  b5: NonTerminal('b5'),
+  s: NonTerminal('s'),
   b6: NonTerminal('b6'),
-  f3: NonTerminal('f3'),
   l: NonTerminal('l'),
+  b5: NonTerminal('b5'),
   assign: NonTerminal('assign'),
+  b4: NonTerminal('b4'),
   Substitute: NonTerminal('Substitute'),
+  b3: NonTerminal('b3'),
   rule: NonTerminal('rule'),
   b2l: NonTerminal('b2l'),
   logic: NonTerminal('logic'),
   b2_not: NonTerminal('b2_not'),
+  b2: NonTerminal('b2'),
   bool: NonTerminal('bool'),
   e: NonTerminal('e'),
   ep: NonTerminal('ep'),
   t: NonTerminal('t'),
   tp: NonTerminal('tp'),
+  f3: NonTerminal('f3'),
   rem: NonTerminal('rem'),
   f2: NonTerminal('f2'),
   f1: NonTerminal('f1'),
   pow: NonTerminal('pow'),
   f0: NonTerminal('f0'),
-  ptn: NonTerminal('ptn'),
-  f: NonTerminal('f'),
-  id_ext: NonTerminal('id_ext'),
-  params_ext: NonTerminal('params_ext'),
+  pattern_ext: NonTerminal('pattern_ext'),
+  pattern_op: NonTerminal('pattern_op'),
+  compound: NonTerminal('compound'),
+  compound_ext: NonTerminal('compound_ext'),
+  compound_ext_2: NonTerminal('compound_ext_2'),
   base: NonTerminal('base'),
-  Num: NonTerminal('Num'),
-  num_ext: NonTerminal('num_ext'),
+  Number: NonTerminal('Number'),
+  number_ext: NonTerminal('number_ext'),
   dot_ext: NonTerminal('dot_ext'),
   scientific_ext: NonTerminal('scientific_ext'),
-  factorial: NonTerminal('factorial'),
-  dfactorial: NonTerminal('dfactorial'),
+
   exclamation: Terminal('exclamation'),
   equal: Terminal('equal'),
   eof: Terminal('eof'),
@@ -114,24 +113,28 @@ export const terminalSymbols: SyntaxSymbol[] = ArrayHelper.toArray(sbl).filter(
 export const allRules: ProductionRule[] = [
   {
     name: 's -> b5',
-    lhs: sbl.start,
+    lhs: sbl.s,
     rhs: [sbl.b5],
   },
+
   {
     name: 'b6 -> b5 l',
     lhs: sbl.b6,
     rhs: [sbl.b5, sbl.l],
   },
+
   {
     name: 'l -> eps',
     lhs: sbl.l,
     rhs: [],
   },
+
   {
     name: 'l -> , b5 l',
     lhs: sbl.l,
     rhs: [sbl.comma, sbl.b5, sbl.l],
   },
+
   {
     name: 'b5 -> b4 assign',
     lhs: sbl.b5,
@@ -167,325 +170,252 @@ export const allRules: ProductionRule[] = [
     lhs: sbl.Substitute,
     rhs: [sbl.substitute, sbl.b3, sbl.Substitute],
   },
+
   {
     name: 'sub -> eps',
     lhs: sbl.Substitute,
     rhs: [],
   },
+
   {
     name: 'b3 -> b2l rule',
     lhs: sbl.b3,
     rhs: [sbl.b2l, sbl.rule],
   },
+
   {
     name: 'rule -> -> b2l',
     lhs: sbl.rule,
     rhs: [sbl.rightArrow, sbl.b2l],
   },
+
   {
     name: 'rule -> :-> b2l',
     lhs: sbl.rule,
     rhs: [sbl.columnRightArrow, sbl.b2l],
   },
+
   {
     name: 'rule -> eps',
     lhs: sbl.rule,
     rhs: [],
   },
+
   {
     name: 'b2l -> b2_not logic',
     lhs: sbl.b2l,
     rhs: [sbl.b2_not, sbl.logic],
   },
+
   {
     name: 'logic -> eps',
     lhs: sbl.logic,
     rhs: [],
   },
+
   {
     name: 'logic -> && b2_not logic',
     lhs: sbl.logic,
     rhs: [sbl.and, sbl.b2_not, sbl.logic],
   },
+
   {
     name: 'logic -> || b2_not logic',
     lhs: sbl.logic,
     rhs: [sbl.or, sbl.b2_not, sbl.logic],
   },
+
   {
     name: 'b2_not -> ! b2_not',
     lhs: sbl.b2_not,
     rhs: [sbl.exclamation, sbl.b2_not],
   },
+
   {
     name: 'b2_not -> b2',
     lhs: sbl.b2_not,
     rhs: [sbl.b2],
   },
+
   {
     name: 'b2 -> e bool',
     lhs: sbl.b2,
     rhs: [sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> > e bool',
     lhs: sbl.bool,
     rhs: [sbl.rightAngle, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> < e bool',
     lhs: sbl.bool,
     rhs: [sbl.leftAngle, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> >= e bool',
     lhs: sbl.bool,
     rhs: [sbl.rightAngleEqual, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> <= e bool',
     lhs: sbl.bool,
     rhs: [sbl.leftAngleEqual, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> == e bool',
     lhs: sbl.bool,
     rhs: [sbl.bool, sbl.doubleEqual, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> === e bool',
     lhs: sbl.bool,
     rhs: [sbl.tripleEqual, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> != e bool',
     lhs: sbl.bool,
     rhs: [sbl.notEqual, sbl.e, sbl.bool],
   },
+
   {
     name: 'bool -> !== e bool',
     lhs: sbl.bool,
     rhs: [sbl.notStrictEqual, sbl.e, sbl.bool],
   },
+
+  {
+    name: 'bool -> eps',
+    lhs: sbl.bool,
+    rhs: [],
+  },
+
   {
     name: 'e -> t ep',
     lhs: sbl.e,
     rhs: [sbl.t, sbl.ep],
   },
+
   {
     name: 'ep -> + t ep',
     lhs: sbl.ep,
     rhs: [sbl.plus, sbl.t, sbl.ep],
   },
+
   {
     name: 'ep -> - t ep',
     lhs: sbl.ep,
     rhs: [sbl.minus, sbl.t, sbl.ep],
   },
+
   {
     name: 'ep -> eps',
     lhs: sbl.ep,
     rhs: [],
   },
+
   {
     name: 't -> f3 tp',
     lhs: sbl.t,
     rhs: [sbl.f3, sbl.tp],
   },
+
   {
     name: 'tp -> * f3 tp',
     lhs: sbl.tp,
     rhs: [sbl.times, sbl.f3, sbl.tp],
   },
+
   {
     name: 'tp -> / f3 tp',
     lhs: sbl.tp,
     rhs: [sbl.divide, sbl.f3, sbl.tp],
   },
+
   {
     name: 'tp -> eps',
     lhs: sbl.tp,
     rhs: [],
   },
+
   {
     name: 'f3 -> f2 rem',
     lhs: sbl.f3,
     rhs: [sbl.f2, sbl.rem],
   },
+
   {
     name: 'rem -> % f2 rem',
     lhs: sbl.rem,
     rhs: [sbl.percent, sbl.f2, sbl.rem],
   },
+
   {
     name: 'rem -> eps',
     lhs: sbl.rem,
     rhs: [],
   },
+
   {
     name: 'f2 -> - f1',
     lhs: sbl.f2,
     rhs: [sbl.minus, sbl.f1],
   },
+
   {
     name: 'f2 -> f1',
     lhs: sbl.f2,
     rhs: [sbl.f1],
   },
+
   {
     name: 'f1 -> f0 pow',
     lhs: sbl.f1,
     rhs: [sbl.f0, sbl.pow],
   },
+
   {
     name: 'pow -> eps',
     lhs: sbl.pow,
     rhs: [],
   },
+
   {
     name: 'pow -> ^ f0 pow',
     lhs: sbl.pow,
     rhs: [sbl.pow, sbl.f0, sbl.pow],
   },
+
   {
-    name: 'f0 -> f ptn',
+    name: 'f0 -> pattern_op pattern_ext',
     lhs: sbl.f0,
-    rhs: [sbl.f, sbl.ptn],
+    rhs: [sbl.pattern_op, sbl.pattern_ext],
   },
+
   {
-    name: 'f0 -> _ f',
+    name: 'f0 -> compound pattern_op pattern_ext',
     lhs: sbl.f0,
-    rhs: [sbl.singleUnderline, sbl.f, sbl.ptn],
+    rhs: [sbl.compound, sbl.pattern_op, sbl.pattern_ext],
   },
+
   {
-    name: 'f0 -> __ f',
-    lhs: sbl.f0,
-    rhs: [sbl.doubleUnderline, sbl.f, sbl.ptn],
-  },
-  {
-    name: 'f0 -> ___ f',
-    lhs: sbl.f0,
-    rhs: [sbl.tripleUnderline, sbl.f, sbl.ptn],
-  },
-  {
-    name: 'ptn -> eps',
-    lhs: sbl.ptn,
+    name: 'pattern_ext -> eps',
+    lhs: sbl.pattern_ext,
     rhs: [],
   },
+
   {
-    name: 'ptn -> _ f ptn',
-    lhs: sbl.ptn,
-    rhs: [sbl.singleUnderline, sbl.f, sbl.ptn],
+    name: 'pattern_ext -> compound',
+    lhs: sbl.pattern_ext,
+    rhs: [sbl.compound],
   },
-  {
-    name: 'ptn -> __ f ptn',
-    lhs: sbl.ptn,
-    rhs: [sbl.doubleUnderline, sbl.f, sbl.ptn],
-  },
-  {
-    name: 'ptn -> ___ f ptn',
-    lhs: sbl.ptn,
-    rhs: [sbl.tripleUnderline, sbl.f, sbl.ptn],
-  },
-  {
-    name: 'f -> id id_ext',
-    lhs: sbl.f,
-    rhs: [sbl.identifier, sbl.id_ext],
-  },
-  {
-    name: 'id_ext -> eps',
-    lhs: sbl.id_ext,
-    rhs: [],
-  },
-  {
-    name: 'id_ext -> [ params_ext',
-    lhs: sbl.id_ext,
-    rhs: [sbl.leftSquare, sbl.params_ext],
-  },
-  {
-    name: 'params_ext -> l ] id_ext',
-    lhs: sbl.params_ext,
-    rhs: [sbl.l, sbl.rightSquare, sbl.id_ext],
-  },
-  {
-    name: 'params_ext -> [ s ] ] id_ext',
-    lhs: sbl.params_ext,
-    rhs: [
-      sbl.leftSquare,
-      sbl.start,
-      sbl.rightSquare,
-      sbl.rightSquare,
-      sbl.id_ext,
-    ],
-  },
-  {
-    name: 'base -> ( e )',
-    lhs: sbl.base,
-    rhs: [sbl.leftParentheses, sbl.e, sbl.rightParentheses],
-  },
-  {
-    name: 'base -> Num',
-    lhs: sbl.base,
-    rhs: [sbl.Num],
-  },
-  {
-    name: 'base -> { L }',
-    lhs: sbl.base,
-    rhs: [sbl.leftBracket, sbl.l, sbl.rightBracket],
-  },
-  {
-    name: 'base -> <| L |>',
-    lhs: sbl.base,
-    rhs: [sbl.leftAngleColumn, sbl.l, sbl.columnRightAngle],
-  },
-  {
-    name: 'base -> str',
-    lhs: sbl.base,
-    rhs: [sbl.string],
-  },
-  {
-    name: 'base -> identifier',
-    lhs: sbl.base,
-    rhs: [sbl.identifier],
-  },
-  {
-    name: 'Num -> num num_ext',
-    lhs: sbl.Num,
-    rhs: [sbl.number, sbl.num_ext],
-  },
-  {
-    name: 'num_ext -> factorial',
-    lhs: sbl.num_ext,
-    rhs: [sbl.factorial],
-  },
-  {
-    name: 'num_ext -> . num dot_ext',
-    lhs: sbl.num_ext,
-    rhs: [sbl.dot, sbl.number, sbl.dot_ext],
-  },
-  {
-    name: 'dot_ext -> eps',
-    lhs: sbl.dot_ext,
-    rhs: [],
-  },
-  {
-    name: 'dot_ext -> e scientific_ext',
-    lhs: sbl.dot_ext,
-    rhs: [sbl.e, sbl.scientific_ext],
-  },
-  {
-    name: 'factorial -> eps',
-    lhs: sbl.factorial,
-    rhs: [],
-  },
-  {
-    name: 'dfactorial -> eps',
-    lhs: sbl.dfactorial,
-    rhs: [],
-  },
-  {
-    name: 'dfactorial -> !',
-    lhs: sbl.dfactorial,
-    rhs: [sbl.exclamation],
-  },
+
+  
 ];
